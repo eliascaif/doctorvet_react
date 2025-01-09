623278565a3edcfd095b8c7c7c5635e4eca8d9e5
Florencia <florzoni@gmail.com>
2025-01-09 12:09:01 -0300

    Agregué solo archivo EditPetRace

--- src/pages/edit/EditPetRace.jsx
+++ src/pages/edit/EditPetRace.jsx
@@ -1,31 +1,33 @@
-/* import { useRef, useState, useEffect } from "react";
-import { AppBarProvider } from "../../providers/AppBarProvider";
+import { useRef, useState, useEffect } from "react";
+import { TextField, Autocomplete, Fab, Container, Box } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { useAppBar } from "../../providers/AppBarProvider";
 import { useSnackbar } from "../../providers/SnackBarProvider";
 import { useNavigate } from "react-router-dom";
 import { useLoading } from "../../providers/LoadingProvider";
 import { strings } from "../../constants/strings";
 import axios from "axios";
 import * as lib from "../../utils/lib";
+import PropTypes from "prop-types";
 
-export const EditPetRace = ({ updatePetRace = null }) => {
+const EditPetRace = ({ updatePetRace = null }) => {
   const [petRace, setPetRace] = useState(
     updatePetRace || {
-      name: "",
+      race: "",
       especie: null,
     }
   );
   const [errors, setErrors] = useState({});
   const [refs, setRefs] = useState({
-    name: useRef(null),
+    race: useRef(null),
     especie: useRef(null),
   });
   const [especies, setEspecies] = useState([]);
   const navigate = useNavigate();
-  const { updateTitle } = AppBarProvider();
+  const { updateTitle } = useAppBar();
   const snackbar = useSnackbar();
   const { isLoading, setIsLoading } = useLoading();
 
-  //useEffect
   useEffect(() => {
     if (updatePetRace) {
       updateTitle("", strings.updatePetRace, strings.complete_data);
@@ -38,7 +40,7 @@ export const EditPetRace = ({ updatePetRace = null }) => {
     const fetchPetRace = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_API_URL}/pets_races`,
+          `${import.meta.env.VITE_API_URL}pets_especies`,
           { withCredentials: true }
         );
 
@@ -58,8 +60,6 @@ export const EditPetRace = ({ updatePetRace = null }) => {
     fetchPetRace();
   }, [updatePetRace]);
 
-  //handleChange
-
   const handleChange = (e) => {
     const { name, value } = e.target;
     setPetRace({ ...petRace, [name]: value });
@@ -68,9 +68,9 @@ export const EditPetRace = ({ updatePetRace = null }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (
-      (!lib.validateNonEmpty("name", petRace.name),
+      (!lib.validateNonEmpty("race", petRace.race,
       setErrors,
-      refs.name || !lib.validateNonEmpty("especies", petRace.especies),
+      refs.name) || !lib.validateNonEmpty("especies", petRace.especies),
       setErrors,
       refs.name)
     ) {
@@ -88,7 +88,23 @@ export const EditPetRace = ({ updatePetRace = null }) => {
     setIsLoading(true);
     try {
       const response = await axios.put(
-        `{import.meta.env.VITE_API_URL}/pets_races`,
+        `${import.meta.env.VITE_API_URL}pets_races`,
+        { withCredentials: true }
+      );
+      navigate(`/main/view-pet-race`, { state: { id: response.data.data.id } });
+    } catch (error) {
+      lib.handleError(error);
+      snackbar("Error al actualizar la raza");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const save = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}pets_races`,
         { withCredentials: true }
       );
       navigate(`/main/view-pet-race`, { state: { id: response.data.data.id } });
@@ -100,6 +116,61 @@ export const EditPetRace = ({ updatePetRace = null }) => {
     }
   };
 
-  return <>EditPetRace</>;
+  return (
+    <Container>
+      <Box sx={{ mb: 4 }} component="form" onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          margin="normal"
+          label={`${strings.race} *`}
+          name="name"
+          value={petRace.race}
+          onChange={handleChange}
+          inputRef={refs.race}
+          error={!!errors.race}
+          helperText={errors.race}
+        />
+        <Autocomplete
+          fullWidth
+          options={especies}
+          getOptionLabel={(option) => (option ? option.name : "")}
+          value={petRace.especie}
+          onChange={(e, newValue) =>
+            setPetRace({
+              ...petRace,
+              especie: newValue,
+            })
+          }
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label={`${strings.especie} *`}
+              name="especie"
+              margin="normal"
+              inputRef={refs.especie}
+              error={!!errors.especie}
+              helperText={errors.especie}
+            />
+          )}
+        />
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 16,
+            right: 16,
+            zIndex: 1000,
+          }}
+        >
+          <Fab color="primary" aria-label="save" onClick={handleSubmit}>
+            <CheckIcon />
+          </Fab>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
- */
+EditPetRace.propTypes = {
+  updatePetRace: PropTypes.func, 
+};
+
+export default EditPetRace;
